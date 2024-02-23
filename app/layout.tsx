c93bf101f4b11f658226b3afb7350412820ff571
Berke Efe <berkefeoz@icloud.com>
2024-02-23 22:44:03 +0300

    Refactor layout styles for improved visual aesthetics
    Update layout styles to incorporate backdrop-filter for a blurred effect, enhancing the visual appeal of the chat interface. This change provides a more refined and modern user experience. No associated issues.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         )}
       >
         <div className="flex flex-col min-h-screen">
-          <header className="sticky bg-white top-0 w-full py-6 px-4 shadow-sm">
+          <header className="sticky top-0 w-full py-6 px-4 shadow-sm backdrop-filter backdrop-blur-md">
             <div className="flex flex-row justify-between items-center 2xl:w-[1024px] 2xl:mx-auto">
               <h1 className="font-bold text-3xl">Chat</h1>
               <LayoutSheet />
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatContainer from "@/components/chat/chat-container";
 
 export default function Home() {
   return (
-    <main className="p-4 2xl:p-0 flex-1 flex flex-col justify-between space-y-4 2xl:max-w-[1024px] 2xl:m-auto w-full">
+    <main className="p-4 2xl:px-0 flex-1 flex flex-col justify-between space-y-4 2xl:max-w-[1024px] 2xl:m-auto w-full">
       <ChatContainer />
     </main>
   );
